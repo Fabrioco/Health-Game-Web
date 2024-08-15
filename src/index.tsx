0e2817a4f0af64559ba605dfbf54e4816f4ed99a
Fabricio Lopes <fabriciooliveiralopes50@gmail.com>
2024-08-15 20:32:49 -0300

    feat: pagina de login estilizada

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./app/App";
-import "./global.css";
+import "./assets/styles/global.css";
+import './assets/styles/root.css';
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+import logo from "../../assets/images/logo.png";
+import "./homeStyles.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="container">
+      <div className="content">
+        <img src={logo} alt="logo.png" className="logo" />
+        <div className="buttons">
+          <a href="/login" className="login-link-button">
+            Sign In
+          </a>
+          <a href="/regsiter" className="register-link-button">
+            Sign Up
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 }
